0d7e0c204890fc109d0e3b5bdb635a5f014ce13c
chematelegram <chfondia@gmail.com>
2024-05-10 13:47:39 +0200

    zippo: Initial adaptation for new ROM sources
     * yet the overlays must be cleaned

--- lineage_zippo.mk
+++ lineage_zippo.mk
@@ -7,17 +7,15 @@
 $(call inherit-product, device/lenovo/zippo/device.mk)
 
 # Inherit some common EvolutionX stuff.
-EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BUILD_APERTURE_CAMERA := true
-TARGET_SUPPORTS_QUICK_TAP := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Lenovo
 PRODUCT_DEVICE := zippo
 PRODUCT_MANUFACTURER := Lenovo
 PRODUCT_MODEL := Lenovo L78051
-PRODUCT_NAME := evolution_zippo
+PRODUCT_NAME := lineage_zippo
 
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
